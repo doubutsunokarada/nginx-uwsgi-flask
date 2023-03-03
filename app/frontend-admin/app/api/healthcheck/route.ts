7dacb2420ad193f9534b1f1c022fb2141ab6ae01
Chiaki Abe <chichichiaki228@gmail.com>
2023-03-04 02:00:51 +0900

    modify healthcheck api

--- app/frontend-admin/app/api/healthcheck/route.ts
+++ app/frontend-admin/app/api/healthcheck/route.ts
@@ -1,5 +1,6 @@
 export async function GET(request: Request) {
-  return new Response("", {
+  return new Response(JSON.stringify({ ok: Date.now().toString() }), {
     status: 200,
+    headers: { "Content-Type": "application/json" },
   });
 }
