d62b2edef7ddaf55101a87069a01991b61047b98
Chiaki Abe <chichichiaki228@gmail.com>
2023-03-04 03:05:55 +0900

    reset

--- app/frontend-admin/app/api/healthcheck/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(request: Request) {
-  return new Response(JSON.stringify({ ok: Date.now().toString() }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
